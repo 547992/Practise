e841f6fe8d5c32173a2bb4106b89c9d1472131eb
547992 <tinton.abraham@cognizant.com>
2024-07-04 12:57:57 +0100

    Assignment Submission by Tinton Abraham

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Branching on my-question-6 page
+ router.post('/my-question-6-answer', function (request, response) {
+   let stay = request.session.data['propertype']
+    if (stay == "House"){
+    response.redirect("/my-question-6a")
+    } else if (stay == "Flat") {
+    response.redirect("/my-question-7")
+    }else {
+   response.redirect("/my-question-6a")
+    }
+    })
+
+
